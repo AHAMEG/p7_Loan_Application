db9c561130f44b5ed4d22460bfb02334a191df24
Abdelkarim HAMEG <95477687+AHAMEG@users.noreply.github.com>
2022-06-21 13:33:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-mkdir -p ~/.streamlit/ 
- echo "\ 
- [server]\n\ 
- headless = true\n\ 
- port = $PORT\n\ 
- enableCORS = false\n\ 
- \n\ 
- " > ~/.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"abdelkarim.hameg@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
